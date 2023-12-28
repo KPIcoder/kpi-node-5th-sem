1123f3bf245b874d4304250ebb206b85051df5f2
Vlad Shchehlov <vladshcheglov41@gmail.com>
2023-12-28 21:45:00 +0000

    fix: switched t main server

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-// import { router } from './lib/file-router.js';
-import { router as declarativeRouter } from './lib/declarative-router.js';
-import { createHttpServer } from './lib/http-2.js';
+import { router } from './lib/file-router.js';
+// import { router as declarativeRouter } from './lib/declarative-router.js';
+import { createHttpServer } from './lib/http-server.js';
 
-import { getController, postController } from './routes/example.controller.js';
+// import { getController, postController } from './routes/example.controller.js';
 
 const PORT = 8000;
 
-declarativeRouter.addRoute('/example', 'GET', getController);
-declarativeRouter.addRoute('/example', 'POST', postController);
+// declarativeRouter.addRoute('/example', 'GET', getController);
+// declarativeRouter.addRoute('/example', 'POST', postController);
 
-createHttpServer(declarativeRouter.routes, PORT);
+createHttpServer(router, PORT);
