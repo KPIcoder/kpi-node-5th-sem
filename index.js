34c261a96639acd90ddfda0201b24f988fcd791f
Vlad Shchehlov <vladshcheglov41@gmail.com>
2023-12-29 14:20:14 +0000

    fix: finessed file routing server

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 import { router } from './lib/file-router.js';
-// import { router as declarativeRouter } from './lib/declarative-router.js';
 import { createHttpServer } from './lib/http-server.js';
 
-// import { getController, postController } from './routes/example.controller.js';
-
 const PORT = 8000;
-
-// declarativeRouter.addRoute('/example', 'GET', getController);
-// declarativeRouter.addRoute('/example', 'POST', postController);
 
 createHttpServer(router, PORT);
